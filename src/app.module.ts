f26750337a3db81e146f8c3bce060a787696be10
flopez-2020056 <flopez-2020056@kinal.edu.gt>
2023-03-20 10:29:27 -0600

    respaldo de progreso en nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JorValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
-    
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JorValidationSchema,
+
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'), 
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
@@ -24,4 +31,9 @@ import { SeedModule } from './seed/seed.module';
 
   ],
 })
-export class AppModule {}
+export class AppModule {
+
+ 
+
+
+}
